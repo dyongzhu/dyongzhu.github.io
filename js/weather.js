fb09c64c480af1e1068e6b80d34cf983f15e63a6
wavelives <3166629885@qq.com>
2020-02-27 20:39:21 +0800

    Site updated: 2020-02-27 20:39:04

---
+++ js/weather.js
@@ -0,0 +1,6 @@
+<!-- weather -->
+WIDGET = {ID: 'wESp55k4W7'};
+var windowWidth = $(window).width(); 
+if (windowWidth > 768) { 
+document.write('<script type="text/javascript" src="https://apip.weatherdt.com/float/static/js/r.js?v=1111"><\/script>'); 
+}
