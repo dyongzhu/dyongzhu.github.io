d95226804982892eb200e557d4ebe491b404bbb5
dyongzhu <3166629885@qq.com>
2020-03-03 16:00:18 +0800

    Site updated: 2020-03-03 16:00:14

--- js/wenzi.js
+++ js/wenzi.js
@@ -1,1 +1,30 @@
-"use strict";var a_idx=0;jQuery(document).ready(function(i){i("body").click(function(a){var t=new Array("❤浪浪最美❤","❤浪浪最可爱❤","❤浪浪美美哒❤","❤浪浪❤","❤朱浪的浪❤","❤朱浪的朱❤","❤小仙女浪浪❤","❤浪浪小可爱❤","❤可爱哒浪浪❤","❤浪浪小美眉❤","❤朱朱浪浪❤","❤可爱的浪浪❤"),o=i("<span></span>").text(t[a_idx]);a_idx=(a_idx+1)%t.length;var n=a.pageX,e=a.pageY;o.css({"z-index":1e69,top:e-20,left:n,position:"absolute","font-weight":"bold",color:"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"}),i("body").append(o),o.animate({top:e-180,opacity:0},1500,function(){o.remove()})})});
+/* 鼠标点击文字特效 */
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        //var a = new Array("❤富强❤","❤民主❤","❤文明❤","❤和谐❤","❤自由❤","❤平等❤","❤公正❤","❤法治❤","❤爱国❤","❤敬业❤","❤诚信❤","❤友善❤");
+         //var a = new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");
+		 var a = new Array("❤浪浪最美❤","❤浪浪最可爱❤","❤浪浪美美哒❤","❤浪浪❤","❤朱浪的浪❤","❤朱浪的朱❤","❤小仙女浪浪❤","❤浪浪小可爱❤","❤可爱哒浪浪❤","❤浪浪小美眉❤","❤朱朱浪浪❤","❤可爱的浪浪❤")
+        var $i = $("<span></span>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+        1500,
+        function() {
+            $i.remove();
+        });
+    });
+});
